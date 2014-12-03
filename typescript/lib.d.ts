341b10d82fdaf4e38c8fcdff04bbdc62efb0c211
Harald Niesche <harald@niesche.de>
2014-12-04 00:17:08 +0100

    use patched .d.ts to correct ImageData interface

--- typescript/lib.d.ts
+++ typescript/lib.d.ts
@@ -499,6 +499,10 @@ declare var Number: {
     POSITIVE_INFINITY: number;
 }
 
+interface TemplateStringsArray extends Array<string> {
+    raw: string[];
+}
+
 interface Math {
     /** The mathematical constant e. This is Euler's number, the base of natural logarithms. */
     E: number;
@@ -1269,6 +1273,64 @@ declare var Uint8Array: {
 }
 
 /**
+  * A typed array of 8-bit unsigned integer values. The contents are initialized to 0. If the requested number of bytes could not be allocated an exception is raised.
+  */
+interface Uint8ClampedArray extends ArrayBufferView {
+    /**
+      * The size in bytes of each element in the array. 
+      */
+    BYTES_PER_ELEMENT: number;
+
+    /**
+      * The length of the array.
+      */
+    length: number;
+    [index: number]: number;
+
+    /**
+      * Gets the element at the specified index.
+      * @param index The index at which to get the element of the array.
+      */
+    get(index: number): number;
+
+    /**
+      * Sets a value or an array of values.
+      * @param index The index of the location to set.
+      * @param value The value to set.
+      */
+    set(index: number, value: number): void;
+
+    /**
+      * Sets a value or an array of values.
+      * @param array A typed or untyped array of values to set.
+      * @param offset The index in the current array at which the values are to be written.
+      */
+    set(array: Uint8Array, offset?: number): void;
+
+    /**
+      * Sets a value or an array of values.
+      * @param array A typed or untyped array of values to set.
+      * @param offset The index in the current array at which the values are to be written.
+      */
+    set(array: number[], offset?: number): void;
+
+    /**
+      * Gets a new Uint8Array view of the ArrayBuffer Object store for this array, specifying the first and last members of the subarray. 
+      * @param begin The index of the beginning of the array.
+      * @param end The index of the end of the array.
+      */
+    subarray(begin: number, end?: number): Uint8Array;
+}
+declare var Uint8ClampedArray: {
+    prototype: Uint8ClampedArray;
+    new (length: number): Uint8ClampedArray;
+    new (array: Uint8ClampedArray): Uint8ClampedArray;
+    new (array: number[]): Uint8ClampedArray;
+    new (buffer: ArrayBuffer, byteOffset?: number, length?: number): Uint8ClampedArray;
+    BYTES_PER_ELEMENT: number;
+}
+
+/**
   * A typed array of 16-bit integer values. The contents are initialized to 0. If the requested number of bytes could not be allocated an exception is raised.
   */
 interface Int16Array extends ArrayBufferView {
@@ -1943,7 +2005,6 @@ interface Date {
 /////////////////////////////
 /// IE DOM APIs
 /////////////////////////////
-
 
 interface PositionOptions {
     enableHighAccuracy?: boolean;
@@ -9294,7 +9355,7 @@ declare var Event: {
 
 interface ImageData {
     width: number;
-    data: number[];
+    data: Uint8ClampedArray;
     height: number;
 }
 declare var ImageData: {
